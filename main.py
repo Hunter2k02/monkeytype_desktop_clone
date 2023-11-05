d19f778e4d6ef779a9b3f3cf77b0a44c9de8cc53
Filip <philippo.sanecki@gmail.com>
2023-11-05 09:21:32 +0100

    First working prototype of app

--- main.py
+++ main.py
@@ -3,57 +3,83 @@ import time
 import tkinter as tk
 
 
+
+black = "#323437"
+yellow = "#E2B714"
+grey = "#646665"
+transparent = "#000000"
+
+
 def main():
     
     
 
-    black = "#323437"
-    yellow = "#E2B714"
-    grey = "#646665"
-
+    
     root = tk.Tk()
     root.title("MonkeyType Clone")
     root.geometry("800x600")
+    root.resizable(False, False)
     root.configure(bg=black)
     canvas = tk.Canvas(root, width=800, height=600, bg=black)
     canvas.pack()
     canvas.create_text(75, 25, text="monkeytype", fill=yellow, font=("Courier", 14, "italic"))
     
-    
-    
-    
-    Button = tk.Button(root, text="Start", width=10, height=2, bg=grey, fg=yellow, font=("Courier", 14), command=lambda:get_sentence(inputBox))
-    Button.place(x=350, y=450)
-    inputBox = tk.Entry(root, width=47, bg=black, text = get_sentence(), fg=yellow, font=("Courier", 18))
-    inputBox.place(x=75, y=100)
-    
-    
-    
 
-    
-    
+
+    button = tk.Button(root, text="Start", width=10, bg=grey, fg=yellow, font=("Courier", 14, "italic"), command=lambda: reset_sentence(root, canvas, input))
+    button.place(x=350, y=500)
+
+
+    input = tk.Entry(root, width=55, bg=black, fg=yellow, font=("Courier", 14, "italic"))
+    input.place(x=100, y=200)
+    input.focus_set()
+    input.bind("<KeyPress>", lambda event: check_input(root, canvas, input))
+
     root.mainloop()
 
+def check_input(root, canvas, input):
+    global flag
+    global start_time
+    if flag:
+        flag = False
+        start_time = time.time()
+        
+    if input.get() == " ".join(Text):
+        print(start_time, time.time())
+        end_time = time.time()
+        wpm = calculate_wpm(start_time, end_time, len(Text))
+        canvas.create_text(400, 300, text=f"WPM: {wpm}", fill=yellow, font=("Courier", 14, "italic"))
+        flag = True
+        input.delete(0, "end")
+    
+def calculate_wpm(start_time, end_time, sentence_length):
+    time_elapsed = (end_time - start_time) / 60  # convert to minutes
+    words_per_minute = sentence_length / time_elapsed  # assuming an average word length of 5 characters
+    return int(words_per_minute)
+    
     
 
+def reset_sentence(root, canvas, input):
+    global Text
+    global flag
+    flag = True
+    input.delete(0, "end")
+    Text = get_sentence()
+    canvas.delete("all")
+    canvas.create_text(75, 25, text="monkeytype", fill=yellow, font=("Courier", 14, "italic"))
+    canvas.create_text(400, 100, text=Text, fill=yellow, font=("Courier", 14, "italic"))
+
+
+
 def get_sentence() -> str:
-        
-        sentencesForTest = ["The quick brown fox jumps over the lazy dog.",
-                          "The five boxing wizards jump quickly.",
-                            "Pack my box with five dozen liquor jugs.",
-                            "Jackdaws love my big sphinx of quartz.",
-                            "The five boxing wizards jump quickly.",
-                            "How vexingly quick daft zebras jump!",
-                            "Bright vixens jump; dozy fowl quack.",
-                            "Quick zephyrs blow, vexing daft Jim.",
-                            "Sphinx of black quartz, judge my vow.",
-                            "Waltz, bad nymph, for quick jigs vex.",
-                            "Glib jocks quiz nymph to vex dwarf.",
-                            ]
-        text = random.choice(sentencesForTest)
-        
-        
-        return text
+    with open("words.txt", "r") as f:
+        text = f.read()
+    text = text.split("\n")
+    
+    sentance = [random.choice(text).lower() for i in range(5)]
+    
+
+    return sentance
 
 if __name__ == "__main__":
     main()
